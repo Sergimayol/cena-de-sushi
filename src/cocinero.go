7452f3ef7340f8f31e418fad794fa3a5af19e545
unknown <sergi2808@gmail.com>
2022-12-01 21:33:38 +0100

    Update cocinero.go
    Falta añadir los mensajes correspondiente

--- src/cocinero.go
+++ src/cocinero.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const (
-	TO_PRODUCE = 10
+	TO_PRODUCE    = 10
+	RABBITMQ_HOST = "amqp://guest:guest@localhost:5672/"
 )
 
 var (
 	SHUSHI_TYPES = []string{"nigiris de salmó", "sashimis de tonyina", "makis de cranc"}
+	shushis      [TO_PRODUCE]string
 )
 
-type Empty struct{}
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
 
-type Shushi struct {
-	ShushiType string
+func producir() {
+	for i := 0; i < TO_PRODUCE; i++ {
+		shushi := SHUSHI_TYPES[i%len(SHUSHI_TYPES)]
+		shushis[i] = fmt.Sprintf("Produciendo shushi: %s", shushi)
+		log.Printf(" [x] Sent %s\n", shushi)
+	}
+}
+
+func enviar(ch *amqp.Channel, ctx context.Context, q amqp.Queue, err error) {
+	for i := 0; i < len(shushis); i++ {
+		err = ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,
+			amqp.Publishing{
+				DeliveryMode: amqp.Persistent,
+				ContentType:  "text/plain",
+				Body:         []byte(shushis[i]),
+			})
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent %s", shushis[i])
+	}
 }
 
 func failOnError(err error, msg string) {
@@ -26,27 +56,8 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func producir(ch *amqp.Channel, q amqp.Queue) {
-	// Sacar del canal que el canal esta vacio
-	for i := 0; i < TO_PRODUCE; i++ {
-		body := SHUSHI_TYPES[i%len(SHUSHI_TYPES)]
-		err := ch.Publish(
-			"",     // exchange
-			q.Name, // routing key
-			false,  // mandatory
-			false,  // immediate
-			amqp.Publishing{
-				ContentType: "text/plain",
-				Body:        []byte(body),
-			})
-		failOnError(err, "Failed to publish a message")
-		log.Printf(" [x] Sent %s\n", body)
-	}
-	// Avisar al canal de que el canal esta lleno
-}
-
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial(RABBITMQ_HOST)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -55,14 +66,20 @@ func main() {
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		"task_queue", // name
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	producir(ch, q)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	producir()
+
+	enviar(ch, ctx, q, err)
+
 }
