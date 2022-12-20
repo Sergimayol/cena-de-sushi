9f5d375f0e0c8a608cf873a2f83d09810686cc3a
unknown <sergi2808@gmail.com>
2022-12-20 18:39:50 +0100

    Update cocinero.go

--- tests/cocinero.go
+++ tests/cocinero.go
@@ -29,16 +29,16 @@ func failOnError(err error, msg string) {
 func producir() {
 	numRanT1 := rand.Intn(TO_PRODUCE)
 	if numRanT1 != 0 {
-		log.Printf("Preparando %d de %s", numRanT1, SHUSHI_TYPES[0])
+		fmt.Printf("%d de %s\n", numRanT1, SHUSHI_TYPES[0])
 	}
 	restantes := TO_PRODUCE - numRanT1
 	numRanT2 := rand.Intn(restantes)
 	if numRanT2 != 0 {
-		log.Printf("Preparando %d de %s", numRanT2, SHUSHI_TYPES[1])
+		fmt.Printf("%d de %s\n", numRanT2, SHUSHI_TYPES[1])
 	}
 	restantes = restantes - numRanT2
 	if restantes != 0 {
-		log.Printf("Preparando %d de %s", restantes, SHUSHI_TYPES[2])
+		fmt.Printf("%d de %s\n", restantes, SHUSHI_TYPES[2])
 	}
 
 	var shushi = ""
@@ -52,13 +52,13 @@ func producir() {
 		} else {
 			shushi = SHUSHI_TYPES[2]
 		}
-		shushis[i] = fmt.Sprintf("Produciendo shushi: %s", shushi)
-		log.Printf(" [x] Sent %s\n", shushi)
+		shushis[i] = shushi
 	}
 }
 
 func enviar(ch *amqp.Channel, ctx context.Context, q amqp.Queue, err error) {
 	for i := 0; i < len(shushis); i++ {
+		log.Printf("\t[x] Posa dins el plat %s", shushis[i])
 		err = ch.PublishWithContext(ctx,
 			"",     // exchange
 			q.Name, // routing key
@@ -70,7 +70,6 @@ func enviar(ch *amqp.Channel, ctx context.Context, q amqp.Queue, err error) {
 				Body:         []byte(shushis[i]),
 			})
 		failOnError(err, "Failed to publish a message")
-		log.Printf(" [x] Sent %s", shushis[i])
 		// Esperar un tiempo aleatorio entre 1 y 2 segundos
 		//rand.Seed(time.Now().UnixNano())
 		//time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
@@ -78,8 +77,6 @@ func enviar(ch *amqp.Channel, ctx context.Context, q amqp.Queue, err error) {
 }
 
 func avisar(ch *amqp.Channel, ctx context.Context, aviso amqp.Queue, err error) {
-	//	for i := 0; i < TO_PRODUCE; i++ {
-	//body := fmt.Sprintf("Aviso: %d", i)
 	body := fmt.Sprintf("%d", TO_PRODUCE)
 	err = ch.PublishWithContext(ctx,
 		"",         // exchange
@@ -92,7 +89,6 @@ func avisar(ch *amqp.Channel, ctx context.Context, aviso amqp.Queue, err error) 
 			Body:         []byte(body),
 		})
 	failOnError(err, "Failed to publish a message")
-	//}
 }
 
 func main() {
@@ -127,10 +123,12 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	fmt.Print("El cuiner de sushi ja és aquí\nEl cuiner prepararà un plat amb:\n")
+
 	producir()
 
 	enviar(ch, ctx, q, err)
 
 	avisar(ch, ctx, aviso, err)
-	// En vez de enviar N permisos enviar un int con N valor y que cada cliente modifique el valor
+
 }
